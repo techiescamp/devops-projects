05a929c6338c915d090f3086df0c9e7d2ec8604a
scriptcamp <scriptcamp.io@gmail.com>
2023-03-26 11:49:45 +0530

    Update

--- 01-jenkins-setup/terraform/lb-asg/main.tf
+++ 01-jenkins-setup/terraform/lb-asg/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "lb-asg" {
   source = "../modules/lb-asg"
   subnets = ["subnet-058a7514ba8adbb07", "subnet-0dbcd1ac168414927", "subnet-032f5077729435858"]
-  ami_id = "ami-0f7cbb73797d9f23d"
+  ami_id = "ami-03e01b20ea4388ae4"
   instance_type ="t2.small"
   key_name = "techiescamp"
   environment = "dev"
