7e40cd23c667a1ed6608024056bbbe6eba284a32
Aswin1118 <aswinvj390@gmail.com>
2023-06-06 11:18:29 +0530

    [TC-33][update] - made changes in properties.py file

--- 03-scalable-java-app/ansible/files/properties.py
+++ 03-scalable-java-app/ansible/files/properties.py
@@ -1,38 +1,36 @@
 import boto3
 import json
 
-region = 'us-west-2'
-parameter_store = '/dev/petclinic/rds_endpoint'
-secret_name = "/dev/petclinic/db"
-file_path = "/opt/application.properties"
-consul_file_path = "/etc/consul.d/config.hcl"
+def update_configurations():
+    region = 'us-west-2'
+    parameter_store = '/dev/petclinic/rds_endpoint'
+    secret_name = "/dev/petclinic/db"
+    application_properties_path = "/opt/application.properties"
+    consul_config_path = "/etc/consul.d/config.hcl"
 
-ssm = boto3.client('ssm', region_name=region)
+    ssm_client = boto3.client('ssm', region_name=region)
+    secrets_client = boto3.client('secretsmanager', region_name=region)
 
-rds_endpoint = ssm.get_parameter(Name=parameter_store)['Parameter']['Value']
+    rds_endpoint = ssm_client.get_parameter(Name=parameter_store)['Parameter']['Value']
+    database_details = json.loads(secrets_client.get_secret_value(SecretId=secret_name)['SecretString'])
 
-secrets_client = boto3.client('secretsmanager')
+    with open(application_properties_path, 'r') as file:
+        application_file_contents = file.read()
 
-response = secrets_client.get_secret_value(SecretId=secret_name)
+    with open(consul_config_path, 'r') as file:
+        consul_file_contents = file.read()
 
-secret_value = response['SecretString']
+    application_file_contents = application_file_contents.replace("spring.datasource.url=jdbc:mysql://localhost:3306/petclinic", f"spring.datasource.url={rds_endpoint}")
+    application_file_contents = application_file_contents.replace("spring.datasource.username=petclinic", f"spring.datasource.username={database_details['username']}")
+    application_file_contents = application_file_contents.replace("spring.datasource.password=petclinic", f"spring.datasource.password={database_details['password']}")
 
-database_details = json.loads(secret_value)
+    consul_file_contents = consul_file_contents.replace("encrypt = \"{{ encrypt }}\"", f"encrypt={database_details['consul']}")
 
-with open(file_path, 'r') as f:
-    file_contents = f.read()
+    with open(application_properties_path, 'w') as file:
+        file.write(application_file_contents)
 
-with open(consul_file_path, 'r') as f:
-    consul_file = f.read()
+    with open(consul_config_path, 'a') as file:
+        file.write(consul_file_contents)
 
-file_contents = file_contents.replace("spring.datasource.url=jdbc:mysql://localhost:3306/petclinic", f"spring.datasource.url={rds_endpoint}")
-file_contents = file_contents.replace("spring.datasource.username=petclinic", f"spring.datasource.username={database_details['username']}")
-file_contents = file_contents.replace("spring.datasource.password=petclinic", f"spring.datasource.password={database_details['password']}")
-consul_file = consul_file.replace("encrypt = \"{{ encrypt }}\"", f"encrypt={database_details['consul']}")
-
-
-with open(file_path, 'w') as f:
-    f.write(file_contents)
-
-with open(consul_file_path, 'a') as f:
-    f.write(consul_file)
+if __name__ == "__main__":
+    update_configurations()
