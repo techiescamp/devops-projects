ad29bfad3963434e17528e3ad2babb24c25892ae
Aswin1118 <aswinvj390@gmail.com>
2023-07-07 11:16:40 +0530

    [TEC-56][Change] - Added variables

--- 03-scalable-java-app/terraform/rds/main.tf
+++ 03-scalable-java-app/terraform/rds/main.tf
@@ -4,4 +4,9 @@ provider "aws" {
 
 module "rds" {
   source = "../modules/rds"
+  secret_name ="/dev/petclinic/db"
+  db_username = "petclinic"
+  db_name = "petclinic-mysql-rds"
+  db_instance_class = "db.t2.micro"
+  parameter_name = "/dev/petclinic/rds_endpoint"
 }
